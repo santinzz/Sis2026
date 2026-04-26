9bcf1c74172fd264341bfe5bf8eccfeb6911cc00
santinzz <santiagogonuz@gmail.com>
2026-04-26 14:34:07 -0600

    primer commit

---
+++ src/main/java/com/sis/integradorasis2026/Usuario.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.sis.integradorasis2026;
+
+/**
+ *
+ * @author santi
+ */
+public class Usuario {
+    private String nombre;
+    private String apellidos;
+    private Direccion direccion;
+    private String telefonoContacto;
+    private String email;
+    private String tipoUsuario;
+    
+    public Usuario(String nombre, String apellidos, Direccion direccion, String telefonoContacto, String email, String tipoUsuario) 
+    {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefonoContacto = telefonoContacto;
+        this.email = email;
+        this.tipoUsuario = tipoUsuario;
+    } 
+    
+    public String GetNombre()
+    {
+        return nombre;
+    }
+    
+    public void SetNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    
+    public String GetApellidos()
+    {
+        return apellidos;
+    }
+    
+    public void SetApellidos()
+    {
+        this.apellidos = apellidos;
+    }
+    
+    public Direccion GetDireccion()
+    {
+        return direccion;
+    }
+    
+    public void SetDireccion()
+    {
+        this.direccion = direccion;
+    }
+    
+    public String GetTelefonoContacto()
+    {
+        return telefonoContacto;
+    }
+    
+    public void SetTelefonoContacto(String telefonoContacto) 
+    {
+        this.telefonoContacto = telefonoContacto;
+    }
+    
+    public String GetEmail()
+    {
+        return email;
+    }
+    
+    public void SetEmail(String email)
+    {
+        this.email = email;
+    }
+    
+    public String GetTipoUsuario()
+    {
+        return tipoUsuario;
+    }
+    
+    public void SetTipoUsuario()
+    {
+        this.tipoUsuario = tipoUsuario;
+    }
+}
