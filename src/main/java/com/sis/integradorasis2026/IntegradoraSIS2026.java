4fc524acbe65a45eaf1fa5708110731d83af575a
santinzz <santiagogonuz@gmail.com>
2026-04-26 15:06:25 -0600

    agregando integradora

---
+++ src/main/java/com/sis/integradorasis2026/IntegradoraSIS2026.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.sis.integradorasis2026;
+
+import java.util.List;
+
+/**
+ *
+ * @author santi
+ */
+public class IntegradoraSIS2026 {
+
+    public static void main(String[] args) {
+        
+    }
+}
